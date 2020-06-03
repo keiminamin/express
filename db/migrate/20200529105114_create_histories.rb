500b216c49441cae0f782feb5db8e20cc4b4d019
keiminamin <ktaro0512@docomo.ne.jp>
2020-06-03 08:50:53 +0000

    first commit

---
+++ db/migrate/20200529105114_create_histories.rb
@@ -0,0 +1,10 @@
+class CreateHistories < ActiveRecord::Migration[5.2]
+  def change
+    create_table :histories do |t|
+      t.float :x
+      t.float :y
+      t.timestamps null: false
+    end
+
+  end
+end
